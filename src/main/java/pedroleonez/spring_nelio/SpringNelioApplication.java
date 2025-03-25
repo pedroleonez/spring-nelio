3e737ce76da22a2f6c77f2a976bffe20097f58d6
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-03-25 17:39:20 -0300

    first commit

---
+++ src/main/java/pedroleonez/spring_nelio/SpringNelioApplication.java
@@ -0,0 +1,13 @@
+package pedroleonez.spring_nelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedroleonez/spring_nelio/SpringNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package pedroleonez.spring_nelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
