a24093de6c2ca8baa216855e3d6bcb64b9a51f2f
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-03-25 20:13:56 -0300

    feat: add annotation @Table and @GeneratedValue

--- src/main/java/pedroleonez/spring_nelio/entities/UserEntity.java
+++ src/main/java/pedroleonez/spring_nelio/entities/UserEntity.java
@@ -1,8 +1,6 @@
 package pedroleonez.spring_nelio.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,12 +14,14 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "tb_user")
 public class UserEntity implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
