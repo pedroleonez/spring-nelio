a4be3ef20ec3374293dbb7cf2eaaf8ec73594253
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-03-26 16:14:23 -0300

    feat: add methods findAll and findById

--- src/main/java/pedroleonez/spring_nelio/controllers/UserController.java
+++ src/main/java/pedroleonez/spring_nelio/controllers/UserController.java
@@ -1,11 +1,34 @@
 package pedroleonez.spring_nelio.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pedroleonez.spring_nelio.entities.UserEntity;
+import pedroleonez.spring_nelio.services.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserEntity>> findAll() {
+        List<UserEntity> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserEntity> findById(@PathVariable Long id) {
+        UserEntity obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
